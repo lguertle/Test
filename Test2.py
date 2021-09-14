3b6561eee087efe41c25f0099d8a4ef549bb507e
unknown <laurent.gurtler@epfl.ch>
2021-09-14 18:33:20 +0200

    Commit n°2

---
+++ Test2.py
@@ -0,0 +1,10 @@
+def Alea_test(number1, number2):
+    if number1 < number2:
+        print("false")
+    else:
+        print("true")
+
+Alea_test(2,4)
+Alea_test(2,4)
+Alea_test(1,3)
+Alea_test(4,2)
